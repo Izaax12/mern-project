bfbd3bf5a30cde1889c547d026a7637b295a28a2
Isaac Garcia <cob.log.cof@gmail.com>
2024-04-15 13:27:08 -0600

    Create basic pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className='text-3xl text-red-700'>Tu mama me ama</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/SignIn" element={<SignIn/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/Projects" element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
