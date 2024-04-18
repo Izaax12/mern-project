b3e5c8a4eb8f93397c35b7e71f83ef5edded42c9
Isaac Garcia <cob.log.cof@gmail.com>
2024-04-17 23:33:55 -0600

    Se crea API rputw

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config();
 
@@ -10,5 +11,7 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server running on port: 3000')
+    console.log('Server running on port: 3000');
 });
+
+app.use('/api/user', userRoutes);
