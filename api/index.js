ebc734d7fbd54ea41935bdd6612dc49f4f7e4826
Isaac Garcia <cob.log.cof@gmail.com>
2024-04-21 23:26:48 -0600

    Se agrega middleware y una funcion para manejar errores

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 mongoose.connect(process.env.MONGO)
                 .then(()=>{console.log('MongoDb is connected')})
                 .catch((err)=>{console.log(err)});
+
 const app = express();
 
 app.listen(3000, () => {
@@ -19,3 +20,13 @@ app.use(express.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
