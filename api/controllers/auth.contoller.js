82f90a15f860bba31690515aa145aaec68e9fac2
Isaac Garcia <cob.log.cof@gmail.com>
2024-05-02 13:17:41 -0600

    Se crea routa del API de signin

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcrypt'; 
 import bcryptjs from 'bcryptjs'; 
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res, next) =>{
     const { username, email, password } = req.body;
@@ -26,4 +27,26 @@ export const signup = async(req, res, next) =>{
     }catch(error){
        next(error);
     }    
+}
+
+export const signin = async(req, res, next) =>{
+    const {email, password} = req.body;
+    if(!email || !password || email === ' ' || password === ' '){
+        next(errorHandler(400, 'Todos los campos son requeridos'));
+    }
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'Usuario no encontrado!'));
+        }
+        const validPwd = bcryptjs.compareSync(password, validUser.password);
+        if(!validPwd){
+            return next(errorHandler(400, 'Contraseña Invalida!!'));
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET,);
+        const {password: pwd, ...rest} = validUser._doc;
+        res.status(200).cookie('acces token', token,{httpOnly: true,}).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
