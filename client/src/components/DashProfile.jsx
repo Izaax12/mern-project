57b95a093e114aa70b7ec22fa45428219901e74c
Isaac Garcia <cob.log.cof@gmail.com>
2024-06-11 14:11:08 -0600

    Se corrige el form, ahora lo muestra bien

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -7,7 +7,7 @@ export default function DashProfile() {
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Perfil</h1>
-        <form action="flex flex-col gap-4">
+        <form className="flex flex-col gap-4">
             <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
                 <img src={currentUser.profilePicture} alt="user"className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
             </div>
