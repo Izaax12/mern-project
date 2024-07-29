b88db5a988d85ad06e785e7c661e983847aee417
Isaac Garcia <cob.log.cof@gmail.com>
2024-07-29 16:25:37 -0600

    Se completa funcionalidad de subir imagen

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,56 @@
-import { TextInput, Select, FileInput, Button } from "flowbite-react";
+import { TextInput, Select, FileInput, Button, Alert } from "flowbite-react";
+import { useState } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase';
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+ 
 export default function CreatePost() {
+  const [file, setFile] = useState([]);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleUploadImage = async() =>{
+    try {
+      if(!file){
+        setImageUploadError('Porfavor Selecciona una imagen');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = 
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        // eslint-disable-next-line no-unused-vars
+        (error) => {
+          setImageUploadError('La imagen no pudo ser cargada');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData( {...formData, image:downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError('Fallo la carga de la imagen');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Crear post</h1>
@@ -15,11 +63,25 @@ export default function CreatePost() {
           </Select>        
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-          <FileInput type='file' accept='image/*'/>
-          <Button type='button' gradientDuoTone={'purpleToBlue'} size='sm' outline>
-            Subir Imagen
+          <FileInput type='file' accept='image/*' onChange={(e)=>setFile(e.target.files[0])}/>
+          <Button type='button' gradientDuoTone={'purpleToBlue'} 
+          size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress}>
+            {imageUploadProgress ? ( 
+              <div className="w-16 h-16">
+                <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}/>
+              </div>):(
+                'Cargar imagen'
+              )
+            }
           </Button>
         </div>
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+          src={formData.image}
+          alt="carga"
+          className="w-full h-72 object-cover"/>
+        )}
         <ReactQuill theme="snow" placeholder="Escribe Algo...." className="h-72 mb-12" required/>
         <Button type="submit" gradientDuoTone={'purpleToPink'}>
           Publicar
