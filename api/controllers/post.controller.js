596b62d94c339f40b244c9e349a1b84cdc61ed57
Isaac Garcia <cob.log.cof@gmail.com>
2024-08-28 16:55:09 -0600

    Se completa la funcionalidad de crear posts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,16 +2,23 @@ import { errorHandler } from "../utils/error.js";
 import Post from "../models/post.model.js";
 
 export const create = async(req, res, next) => {
+    console.log(req.user)
     if(!req.user.isAdmin){
         return(next(errorHandler(403, 'No tienes los permisos para crear post')));
     }
     if(!req.body.title || !req.body.content){
         return next(errorHandler(400, 'Porfavor proporciona todos los campos requeridos'));
     }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, ' ');
+    const slug = req.body.title
+        .split(' ')
+        .join('-')
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9-]/g, ' ');
+
     const newPost = new Post({
         ...req.body, slug, userId: req.user.id
     });
+    
     try {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,16 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import {app} from '../firebase';
 import {CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
- 
+import{useNavigate} from 'react-router-dom ';
+
 export default function CreatePost() {
   const [file, setFile] = useState([]);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleUploadImage = async() =>{
     try {
@@ -51,15 +55,46 @@ export default function CreatePost() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e. preventDefault();
+    try {
+      const res = await fetch('/api/post/create',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(!res.ok){
+        setPublishError(data.message);
+        return;
+      }
+      if(res.ok){
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      console.log(error);
+      setPublishError('Oh no! algo ha salido mal :(');
+    }
+  }
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Crear post</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput type='text' placeholder='Titulo' required id='title' className="flex-1"/>
-          <Select>
+          <TextInput type='text' 
+            placeholder='Titulo' 
+            required id='title' 
+            className="flex-1"
+            onChange = {(e) => setFormData({...formData, title: e.target.value})}/>
+          <Select
+            onChange={(e) => setFormData({...formData, category: e.target.value})}>
             <option value="uncategorized">Selecciona una categoria</option>
-            <option value="1"></option>
+            <option value="Historia del SW">Historia del SW</option>
+            <option value="Historia del Hw">Historia del HW</option>
           </Select>        
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
@@ -82,10 +117,21 @@ export default function CreatePost() {
           alt="carga"
           className="w-full h-72 object-cover"/>
         )}
-        <ReactQuill theme="snow" placeholder="Escribe Algo...." className="h-72 mb-12" required/>
+
+        <ReactQuill theme="snow" 
+          placeholder="Escribe Algo...." 
+          className="h-72 mb-12" required
+          onChange={(value)=>{
+            setFormData({...formData, content: value})
+          }}
+        />
+        
         <Button type="submit" gradientDuoTone={'purpleToPink'}>
           Publicar
         </Button>
+        {
+          publishError && <Alert color={'failure'}>{publishError}</Alert>
+        }
       </form>
     </div>
   )
