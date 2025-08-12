546f81a00d87f2cb6fe474b90b3e8a90f24f58ac
Isaac Garcia <cob.log.cof@gmail.com>
2025-08-12 16:41:16 -0600

    Create getUsers api

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {test} from '../controllers/user.controller.js';
+import {getUsers, test} from '../controllers/user.controller.js';
 import { updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { deleteUser } from '../controllers/user.controller.js';
@@ -11,5 +11,6 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
